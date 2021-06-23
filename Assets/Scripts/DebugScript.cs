6aa159fd81910c6793cf5d5b09e59039f76e439b
luqingxin <3346717758@qq.com>
2021-06-23 17:22:56 +0800

    基本的地图

---
+++ Assets/Scripts/DebugScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class DebugScript : MonoBehaviour
+{
+    public Tilemap tilemap;
+    public TileBase tile;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Debug.Log(tilemap);
+        //Debug.Log(tilemap.GetSprite(new Vector3Int(-1,-1,0)));
+        //tilemap.SetTile(new Vector3Int(0, 0, 0), tile);
+        //Debug.Log(tilemap.GetTile(new Vector3Int(0, 0, 0)));
+        //Debug.Log(tilemap.tileAnchor);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/mapScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class mapScript : MonoBehaviour
+{
+    public int width, height;
+    public Tilemap tilemap;
+    public MapCellType[,] mapCellTypes = new MapCellType[50,50];
+    public GameObject[,] gameObjects;
+
+    public MapCellType getMapType(Vector3Int v)
+    {
+        return mapCellTypes[v.x, v.y];
+    }
+
+    public void initmap()
+    {
+        for (int i = 1; i <= width; i++)
+        {
+            for (int j = 1; j <= height; j++)
+            {
+                TileBase tile = tilemap.GetTile(new Vector3Int(i, j, 0));
+                if (tile.name == "test_0")
+                {
+                    mapCellTypes[i, j] = MapCellType.obstacle;
+                }
+                else if (tile.name == "test_1")
+                {
+                    mapCellTypes[i, j] = MapCellType.ground;
+                }
+                else if (tile.name == "test_3")
+                {
+                    mapCellTypes[i, j] = MapCellType.trap;
+                }
+            }
+        }
+        /*for (int i = 1; i <= width; i++)
+        {
+            for (int j = 1; j <= height; j++)
+            {
+                Debug.Log(mapTypes[i, j]);
+            }
+        }*/
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        initmap();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
